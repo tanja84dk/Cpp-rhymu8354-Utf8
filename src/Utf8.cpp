2ec74dde38a0cfc7951c0718bd6973e70b1090a4
Richard Walters <rwalters@digitalstirling.com>
2018-07-07 16:22:07 -0700

    Add support for valid non-ASCII Unicode code points

--- src/Utf8.cpp
+++ src/Utf8.cpp
@@ -6,7 +6,30 @@
  * © 2018 by Richard Walters
  */
 
+#include <stddef.h>
 #include <Utf8/Utf8.hpp>
+
+namespace {
+
+    /**
+     * This computes the logarithm (base 2) of the given integer.
+     *
+     * @param[in] integer
+     *     This is the integer for which to compute the logarithm.
+     *
+     * @return
+     *     The logarithm (base 2) of the given integer is returned.
+     */
+    template< typename I > size_t log2n(I integer) {
+        size_t answer = 0;
+        while (integer > 0) {
+            ++answer;
+            integer >>= 1;
+        }
+        return answer;
+    }
+
+}
 
 namespace Utf8 {
 
@@ -33,7 +56,24 @@ namespace Utf8 {
     std::vector< uint8_t > Utf8::Encode(const std::vector< UnicodeCodePoint >& codePoints) {
         std::vector< uint8_t > encoding;
         for (auto codePoint: codePoints) {
-            encoding.push_back((UnicodeCodePoint)(codePoint & 0x7F));
+            const auto numBits = log2n(codePoint);
+            if (numBits <= 7) {
+                encoding.push_back((UnicodeCodePoint)(codePoint & 0x7F));
+            } else if (numBits <= 11) {
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 6) & 0x1F) + 0xC0));
+                encoding.push_back((UnicodeCodePoint)((codePoint & 0x3F) + 0x80));
+            } else if (numBits <= 16) {
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 12) & 0x0F) + 0xE0));
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 6) & 0x3F) + 0x80));
+                encoding.push_back((UnicodeCodePoint)((codePoint & 0x3F) + 0x80));
+            } else if (numBits <= 21) {
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 18) & 0x07) + 0xF0));
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 12) & 0x3F) + 0x80));
+                encoding.push_back((UnicodeCodePoint)(((codePoint >> 6) & 0x3F) + 0x80));
+                encoding.push_back((UnicodeCodePoint)((codePoint & 0x3F) + 0x80));
+            } else {
+                // todo
+            }
         }
         return encoding;
     }
