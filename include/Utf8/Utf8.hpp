334e43128f9be7657a57a5df34812ea2e1e5d4c1
Richard Walters <rwalters@digitalstirling.com>
2018-08-24 13:59:49 -0700

    remove redundant 'struct' from pimpl pattern

--- include/Utf8/Utf8.hpp
+++ include/Utf8/Utf8.hpp
@@ -139,7 +139,7 @@ namespace Utf8 {
         /**
          * This contains the private properties of the instance.
          */
-        std::unique_ptr< struct Impl > impl_;
+        std::unique_ptr< Impl > impl_;
     };
 
 }
